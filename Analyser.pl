ce559724c8ee93d7245470e9591d1ba6696dcf51
lyxorl <80990412+lyxorl@users.noreply.github.com>
2025-02-21 16:55:38 +0100

    Create Draft with winning side and losing side

--- Analyser.pl
+++ Analyser.pl
@@ -3,7 +3,7 @@ process_file(Filename) :-
     open(Filename, read, Stream),
     read_lines(Stream, Lines),
     close(Stream),
-    process_lines(Lines).
+    process_lines(Lines, 0).
 
 read_lines(Stream, []) :-
     at_end_of_stream(Stream).
@@ -12,10 +12,22 @@ read_lines(Stream, [Line | Rest]) :-
     read_line_to_string(Stream, Line),
     read_lines(Stream, Rest).
 
-process_lines([]).
-process_lines([Line | Rest]) :-
+process_lines([], _).
+process_lines([Line | Rest], Id) :-
     atom_string(Nom, Line),
     assertz(champion(Nom)),
-    assertz(champion_id(Nom,0)), %faire l'ajout de 1 par recurrence
-    process_lines(Rest).
+    assertz(champion_id(Nom,Id)),
+    NewID is Id+1,
+    process_lines(Rest, NewID).
 
+draft(Wtop,Wjgl,Wmid,Wadc,Wsup,Ltop,Ljgl,Lmid,Ladc,Lsup) :-
+    champion(Wtop),
+    champion(Wjgl),
+    champion(Wmid),
+    champion(Wadc),
+    champion(Wsup),
+    champion(Ltop),
+    champion(Ljgl),
+    champion(Lmid),
+    champion(Ladc),
+    champion(Lsup).
